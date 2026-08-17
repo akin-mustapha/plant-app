f7c0ca01796f45fbac37d2cc4fcb3acb5dc9f67b
Akinkunmi Mustapha <akinkunmimustapha1@example.com>
2026-08-17 22:36:49 +0100

    add dyanmo db table

--- infra/dynamodb.tf
+++ infra/dynamodb.tf
@@ -8,3 +8,42 @@ resource "aws_dynamodb_table" "plants" {
     type = "S"
   }
 }
+
+resource "aws_dynamodb_table" "activity" {
+  name         = "activity"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "activity_id"
+
+  attribute {
+    name = "activity_id"
+    type = "S"
+  }
+
+  attribute {
+    name = "plant_id"
+    type = "S"
+  }
+
+  attribute {
+    name = "activity_date"
+    type = "S"
+  }
+
+  global_secondary_index {
+    name            = "plant_id-activity_date-index"
+    hash_key        = "plant_id"
+    range_key       = "activity_date"
+    projection_type = "ALL"
+  }
+}
+
+resource "aws_dynamodb_table" "activity_type" {
+  name         = "activity_type"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "activity_type_id"
+
+  attribute {
+    name = "activity_type_id"
+    type = "S"
+  }
+}
